a10095caef81d3e7dac90c09d5582e720e9d2b26
David Englert <david.englert@cern.ch>
2017-07-31 16:25:44 +0100

    Updated c++ template file

--- .vim/template/C++/cpp.cc
+++ .vim/template/C++/cpp.cc
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <fstream>
 
-
-int main ()
+int main (int argc, const char *argv[])
 {
-
+	
+	std::string arg1 =      argv[1];
+	std::string arg2 = atoi(argv[2]);
+	std::string arg3 = atoi(argv[3]);
 
 	return 0;
 }
